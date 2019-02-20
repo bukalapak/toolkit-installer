abde825d69b700969ea0d5d361ccfadc640d162e
Alif Rachmawadi <subosito@bukalapak.com>
2019-02-20 12:36:07 +0700

    added golangci-lint

---
+++ golangci-lint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+LINTER_DIR=${LINTER_DIR:-"./bin"}
+LINTER_BIN="$LINTER_DIR/golangci-lint"
+LINTER_VERSION=${LINTER_VERSION:-"1.15.0"}
+
+download_linter() {
+  curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b "$LINTER_DIR" "v$LINTER_VERSION"
+}
+
+if [ ! -f "$LINTER_BIN" ]; then
+  download_linter
+fi
+
+if ! "$LINTER_BIN" --version | grep -q "$LINTER_VERSION"; then
+  download_linter
+fi
