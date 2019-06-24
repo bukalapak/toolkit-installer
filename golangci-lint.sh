6573dbc43f76e8de23df88c627909ff90b51073b
Alif Rachmawadi <subosito@bukalapak.com>
2019-06-24 07:54:25 +0700

    automatically detect golangci-lint latest version

--- golangci-lint.sh
+++ golangci-lint.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+linter_latest_version() {
+  curl -sL https://api.github.com/repos/golangci/golangci-lint/releases/latest | \
+    grep '"tag_name":' | \
+    awk '{gsub("\"",""); gsub(",",""); gsub("v",""); print $2}'
+}
+
 LINTER_DIR=${LINTER_DIR:-"./bin"}
 LINTER_BIN="$LINTER_DIR/golangci-lint"
-LINTER_VERSION=${LINTER_VERSION:-"1.17.1"}
+LINTER_VERSION=${LINTER_VERSION:-$(linter_latest_version)}
 
 download_linter() {
   curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b "$LINTER_DIR" "v$LINTER_VERSION"
