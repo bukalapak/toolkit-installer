226475963d67f2d20d7b37f8c2bf5eb1def881b5
Bicky Eric Kantona <bickyeric@ymail.com>
2022-03-29 16:25:59 +0700

    specify hardware name

--- buf.sh
+++ buf.sh
@@ -8,7 +8,8 @@ INSTALL_VERSION=${INSTALL_VERSION:-0.24.0}
 
 download() {
   OS_NAME=$(uname -s)
-  DOWNLOAD_URL="https://github.com/bufbuild/buf/releases/download/v${INSTALL_VERSION}/buf-${OS_NAME}-x86_64.tar.gz"
+  HARDWARE_NAME=$(uname -m)
+  DOWNLOAD_URL="https://github.com/bufbuild/buf/releases/download/v${INSTALL_VERSION}/buf-${OS_NAME}-${HARDWARE_NAME}.tar.gz"
   curl -sSL "${DOWNLOAD_URL}" | tar -xvzf - -C "$INSTALL_DIR" --strip-components 2
 }
 
